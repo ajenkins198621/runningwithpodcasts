7f77ccab2c17a5674938601b7b17dcdbd6a75ad5
Austin <austinclarkjenkins@gmail.com>
2017-01-16 18:11:22 -0500

    Add Authentication

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,6 @@ Route::get('/profile/{username}', "ProfileController@show");
 
 Route::get('/runs/{id}',"RunsController@show");
 Route::post('/runs/create',"RunsController@store");
+Auth::routes();
+
+Route::get('/home', 'HomeController@index');
